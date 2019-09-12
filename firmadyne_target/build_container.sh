76f2b7fc503208dd868a27026478f9c214cbd238
mborgerson <contact@mborgerson.com>
2019-09-12 15:18:36 -0700

    Update build_container.sh

--- firmadyne_target/build_container.sh
+++ firmadyne_target/build_container.sh
@@ -27,9 +27,13 @@ set -e
 
 
 # https://dlinkmea.com/index.php/product/details/?det=NmFNY0ZsYnAvN3BYZlA0a0d2blliQT09
-URL=https://dlinkmea.com/upload/downloadable/7636-DSL-2740U_V1_FW_ME_1.02_webupload.rar
-FILE="DSL-2740U V1 FW ME_1.02_webupload.zip"
-NAME="dsl-2740u"
+#URL=https://dlinkmea.com/upload/downloadable/7636-DSL-2740U_V1_FW_ME_1.02_webupload.rar
+#FILE="DSL-2740U V1 FW ME_1.02_webupload.zip"
+#NAME="dsl-2740u"
+
+URL=http://www.downloads.netgear.com/files/GDC/WNAP320/WNAP320%20Firmware%20Version%202.0.3.zip
+ZIP_FILE="WNAP320 Firmware Version 2.0.3.zip"
+NAME=wnap320
 
 # Download firmware image:
 if [ ! -e "$FILE" ]; then
